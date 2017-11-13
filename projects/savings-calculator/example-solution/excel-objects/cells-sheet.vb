c14e9ac79d9f9bdb6f8da3aa806c8677b8a085a6
MJ Rossetti <datacreativellc@gmail.com>
2017-11-13 01:28:34 -0500

    Edit attribution instructions

--- projects/savings-calculator/example-solution/excel-objects/cells-sheet.vb
+++ projects/savings-calculator/example-solution/excel-objects/cells-sheet.vb
@@ -1,7 +1,7 @@
 '
 ' A SOLUTION FOR ALL-CELLS INTERFACE
 ' ... Author: Prof. Rossetti <prof.mj.rossetti@gmail.com>.
-' ... License: Students, feel free but not obligated to use this module in your project as long as you retain this attribution section. If you wrote something like this on your own, no need to attribute. If this code inspired you to write your own code, please still consider providing an attribution link to this file's GitHub URL.
+' ... License: Students, feel free but not obligated to use this code in your project as long as you retain this attribution section. If you wrote something like this on your own, no need to attribute. If this code inspired you to write your own code, please still consider providing an attribution link to this file's GitHub URL.
 '
 
 Private Sub CommandButton1_Click()
